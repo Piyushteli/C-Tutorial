2fd1da5f707180df6d88a5ceac4ff6d90938cbbf
Piyushteli <piyushteli85@gmail.com>
2024-12-02 12:05:32 +0530

    Add files via upload

---
+++ Average_of_3numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//#include<math.h>
+int main()
+{
+    int X,Y,Z;
+    printf("Enter the value of X:");
+    scanf("%d",&X);
+
+    printf("Enter the value of Y:");
+    scanf("%d",&Y);
+
+    printf("Enter the value of Z:");
+    scanf("%d",&Z);
+
+    printf("%d",(X+Y+Z)/3);
+}
---
+++ divisibleby2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    printf("%d",n%2==0);
+
+  /* if (n%2==0)
+    printf("%d is divisible by 2",n);
+
+   else
+    printf("%d is not divisible by 2",n);*/
+
+    
+}
