810b9ed3d81158c7d9a780021d659c34f0bedfe4
Piyushteli <piyushteli85@gmail.com>
2024-12-02 11:57:45 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,23 @@
+//Area of Square
+
+#include <stdio.h>
+int main()
+{
+    int side;
+    printf("Enter the value of side =");
+    scanf("%d",&side);
+
+    printf("Area of Square is %d",side*side);
+
+}
+
+//Area of Circle
+#include <stdio.h>
+int main()
+{
+    float Radius;
+    printf("Enter the value of Radius =");
+    scanf("%f",&Radius);
+
+    printf("Area of Cicle is %f",3.14*Radius*Radius);
+}
---
+++ cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("The value of n is");
+    scanf("%d",&n);
+
+    printf("Cube of %d is %d\n",n,n*n*n);
+}
